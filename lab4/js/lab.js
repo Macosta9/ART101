cd99338aab31e48e565f254095ca48c23f29bda4
Macosta9 <166079096+Macosta9@users.noreply.github.com>
2024-04-26 02:15:26 -0700

    Lab 5 html code

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,8 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//**
+// Sudoku
+// Players of this game will try to solve a puzzle using math and number arragnemnts. Players cannot repeat numbers. 
+  //Creating the bix with tiles (9x9)
+    //putting random numbers into a few tiles
+  // Accept user input
+    // Mark input red if its wring 
+    
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+ * Author: Mariana Acosta <macosta9@ucsc.edu>
+ * Created: 24 Apirl 2-24
+ * Liscense: Public Domain
+ */
 
-// Constants
+// Define Varible
+make = "Ford" 
+model ="Taurus";
+color = "Rusty";
+year = 1995;
 
-// Functions
+// calculate
+age = 2019 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// output
+document.writeIN ("make:"+make+"br>")
+document.writeIN ("Model:" + model + "br>")
+document.writeIN ("Color:" + color + "br>")
+document.writeIN ("Year:" + year + "br>")
+document.writeIN ("Age:" + age + "br>")
